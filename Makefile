717e509a89b9c0d965bbf9d3de641fcef0762429
idavoli- <idavoli-@student.42sp.org.br>
2021-09-01 20:10:41 -0300

    partial commit at tolower norminete OK tests OK

--- Makefile
+++ Makefile
@@ -18,7 +18,9 @@ SRC = ./main.c \
 	./ft_memcpy.c \
 	./ft_memmove.c \
 	./ft_strlcpy.c \
-	./ft_strlcat.c
+	./ft_strlcat.c \
+	./ft_toupper.c \
+	./ft_tolower.c
 
  OBJ = $(SRC:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: idavoli- <idavoli-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 11:27:23 by idavoli-          #+#    #+#             */
-/*   Updated: 2021/09/01 14:21:28 by idavoli-         ###   ########.fr       */
+/*   Updated: 2021/09/01 19:32:56 by idavoli-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
